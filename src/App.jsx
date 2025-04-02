b9f3c7c1ec4710783ffa2d046fc45af9b46e206c
madhu-software <inaparthi.madhukiran.s89@kalvium.community>
2025-04-02 14:29:28 +0530

    a

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Madhu</h1>
+      <h2>contact details : </h2>
+      <h3>email : inaparthi.madhukiran.s89@kalvium.community</h3>
+      <h3>github username : madhu-inaparthi</h3>
+    </>
+  )
+}
+
+export default App
